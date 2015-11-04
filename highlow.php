f2673fab4845eab41639b15385b6d7186e338a91
Thomas Allen <thomasallen@Thomass-MacBook-Pro.local>
2015-11-04 11:21:03 -0600

    Completed high low game in php

--- highlow.php
+++ highlow.php
@@ -1,3 +1,14 @@
 <?php
-
+$random = mt_rand(1, 100);
+fwrite(STDOUT, 'Guess a number between 1 and 100:' . PHP_EOL);
+do {
+	$guess = intVal(fgets(STDIN));
+	if ($guess < $random) {
+		fwrite(STDOUT, 'HIGHER' . PHP_EOL);
+	} else if ($guess > $random){
+		fwrite(STDOUT, 'LOWER' . PHP_EOL);
+	} else {
+		fwrite(STDOUT, 'Lucky guess! You win!');
+	}	
+} while ($guess != $random);
 ?>
